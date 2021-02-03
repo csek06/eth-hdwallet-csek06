0e2d571c144d931cf8e5d690044e8b1a5c101662
csek06 <sechlerc@mail.gvsu.edu>
2021-02-02 23:48:11 -0500

    update to run as python3

--- firstrun.sh
+++ firstrun.sh
@@ -13,16 +13,19 @@ echo "Checking Container Type"
 if [ ! -z $TYPE ]; then
 	DIR="/config"
 	if [ $TYPE = "SEED" ]; then
+		echo "Running as a seed creator"
 		SCRIPT_FILE="$DIR/mongo-seed-creator.py"
 	fi
 	if [ $TYPE = "ADDRESS" ]; then
+		echo "Running as an address checker"
 		SCRIPT_FILE="$DIR/mongo-address-lookup.py"
 	fi
 	if [ $TYPE = "BALANCE" ]; then
+		echo "Running as an balance checker"
 		SCRIPT_FILE="$DIR/mongo-balance-lookup.py"
 	fi
 	if [ -f $SCRIPT_FILE ]; then
-		python $SCRIPT_FILE
+		python3 $SCRIPT_FILE
 	else
 		echo "Script file not found... $SCRIPT_FILE"
 	fi
