809ca830ee6705ef2001b913ff37e9859745b730
csek06 <sechlerc@mail.gvsu.edu>
2021-05-12 11:10:32 -0400

    add go seedcreator

--- firstrun.sh
+++ firstrun.sh
@@ -17,44 +17,40 @@ if [ ! -z $TYPE ]; then
 		SCRIPT_FILE="$DIR/mongo-go-address-lookup_linux"
 		chmod +x $SCRIPT_FILE
 		$SCRIPT_FILE
+	elif [ $TYPE = "GO_SEED" ]; then
+		echo "Running as a Golang Seed creator"
+		SCRIPT_FILE="$DIR/mongo-go-seed-creator_linux"
+		chmod +x $SCRIPT_FILE
+		$SCRIPT_FILE
 	else
 		if [ $TYPE = "SEED" ]; then
 			echo "Running as a seed creator"
 			SCRIPT_FILE="$DIR/mongo-seed-creator.py"
-		fi
-		if [ $TYPE = "ITERATOR" ]; then
+		elif [ $TYPE = "ITERATOR" ]; then
 			echo "Running as a iterator list creator"
 			SCRIPT_FILE="$DIR/mongo-iterator-creator.py"
-		fi
-		if [ $TYPE = "ADDRESS" ]; then
+		elif [ $TYPE = "ADDRESS" ]; then
 			echo "Running as an address checker"
 			SCRIPT_FILE="$DIR/mongo-address-lookup.py"
-		fi
-		if [ $TYPE = "BALANCE" ]; then
+		elif [ $TYPE = "BALANCE" ]; then
 			echo "Running as a balance checker"
 			SCRIPT_FILE="$DIR/mongo-balance-lookup.py"
-		fi
-		if [ $TYPE = "ETHERSCAN" ]; then
+		elif [ $TYPE = "ETHERSCAN" ]; then
 			echo "Running as an etherscan balance checker"
 			SCRIPT_FILE="$DIR/mongo-balance-lookup-etherscan.py"
-		fi
-		if [ $TYPE = "OPENETHEREUM" ]; then
+		elif [ $TYPE = "OPENETHEREUM" ]; then
 			echo "Running as an openethereum balance checker"
 			SCRIPT_FILE="$DIR/mongo-balance-lookup-openethereum.py"
-		fi
-		if [ $TYPE = "BALANCE_CROSSCHECK" ]; then
+		elif [ $TYPE = "BALANCE_CROSSCHECK" ]; then
 			echo "Running as a mongodb balance crosschecker"
 			SCRIPT_FILE="$DIR/mongo-balance-crosscheck.py"
-		fi
-		if [ $TYPE = "WORKER_ADMIN" ]; then
+		elif [ $TYPE = "WORKER_ADMIN" ]; then
 			echo "Running as a mongodb worker admin"
 			SCRIPT_FILE="$DIR/mongo-worker-admin.py"
-		fi
-		if [ $TYPE = "BALANCES_UPDATER" ]; then
+		elif [ $TYPE = "BALANCES_UPDATER" ]; then
 			echo "Running as a mongodb balances updater"
 			SCRIPT_FILE="$DIR/mongo-bigquery-getbalances.py"
-		fi
-		if [ -f $SCRIPT_FILE ]; then
+		elif [ -f $SCRIPT_FILE ]; then
 			python3 $SCRIPT_FILE
 		else
 			echo "Script file not found... $SCRIPT_FILE"
